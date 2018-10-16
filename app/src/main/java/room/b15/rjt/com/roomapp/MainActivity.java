eb1230bef00a413a0de8870a4e4d4a279a21d910
ansari <abdul.tanveer@gmail.com>
2018-10-17 01:02:30 +0530

    inserting data into db using asynctask

--- app/src/main/java/room/b15/rjt/com/roomapp/MainActivity.java
+++ app/src/main/java/room/b15/rjt/com/roomapp/MainActivity.java
@@ -1,5 +1,6 @@
 package room.b15.rjt.com.roomapp;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -23,6 +24,27 @@ public class MainActivity extends AppCompatActivity {
         EditText wordEditText = findViewById(R.id.word_edittext);
         String data =  wordEditText.getText().toString();
         Word word = new Word(data);
-        mWordDao.insert(word);
+        insert(word);
+        wordEditText.setText("");
+       // mWordDao.insert(word);
+    }
+
+    public void insert (Word word) {
+        new insertAsyncTask(mWordDao).execute(word);
+    }
+
+    private static class insertAsyncTask extends AsyncTask<Word, Void, Void> {
+
+        private WordDao mAsyncTaskDao;
+
+        insertAsyncTask(WordDao dao) {
+            mAsyncTaskDao = dao;
+        }
+
+        @Override
+        protected Void doInBackground(final Word... params) {
+            mAsyncTaskDao.insert(params[0]);
+            return null;
+        }
     }
 }
