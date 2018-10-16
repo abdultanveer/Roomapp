b9fcb1b73c915aa8ddbd26b2e4f542753f7a7e06
ansari <abdul.tanveer@gmail.com>
2018-10-16 23:46:37 +0530

    Initial commit

---
+++ app/src/main/java/room/b15/rjt/com/roomapp/MainActivity.java
@@ -0,0 +1,20 @@
+package room.b15.rjt.com.roomapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void clickHandler(View v){
+        //EditText wordEditText = findViewById(R.id.)
+
+    }
+}
