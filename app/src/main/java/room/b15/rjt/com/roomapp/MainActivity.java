ad4ef21f51eef85e1387bd57e337de297fb932d8
ansari <abdul.tanveer@gmail.com>
2018-10-16 23:51:51 +0530

    inserting data into db

--- app/src/main/java/room/b15/rjt/com/roomapp/MainActivity.java
+++ app/src/main/java/room/b15/rjt/com/roomapp/MainActivity.java
@@ -6,15 +6,23 @@ import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    WordRoomDatabase db;
+    private WordDao mWordDao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+         db = WordRoomDatabase.getDatabase(this);
+         mWordDao = db.wordDao();
+
+
     }
 
     public void clickHandler(View v){
-        //EditText wordEditText = findViewById(R.id.)
-
+        EditText wordEditText = findViewById(R.id.word_edittext);
+        String data =  wordEditText.getText().toString();
+        Word word = new Word(data);
+        mWordDao.insert(word);
     }
 }
