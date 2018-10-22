03267a71c60b88d82fcbc7b32a747835301518ef
ansari <abdul.tanveer@gmail.com>
2018-10-23 01:08:11 +0530

    custom view

---
+++ app/src/main/java/room/b15/rjt/com/roomapp/CustomviewActivity.java
@@ -0,0 +1,13 @@
+package room.b15.rjt.com.roomapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CustomviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customview);
+    }
+}
